1ebd7be9e55e7416879bcb4e9f6216e9629eee42
Fernando Mendonca Maranho <maranhofm@tcu.gov.br>
2016-11-07 17:19:02 -0200

    corrigindo problema de multiplos ng-select abertos em uma mesma página

--- components/select/off-click.ts
+++ components/select/off-click.ts
@@ -9,7 +9,14 @@ export class OffClickDirective implements OnInit, OnDestroy {
   @Input('offClick') public offClickHandler: any;
   /* tslint:enable */
   @HostListener('click', ['$event']) public onClick($event: MouseEvent): void {
-    $event.stopPropagation();
+    //$event.stopPropagation();
+	
+	// Permite propagar o evento de clique para fechar outras eventuais combos que estejam abertas.
+	// Para não fechar a combo atual, remove temporariamente o handler responsável por fechar a combo atual a partir de um evento de clique.
+	document.removeEventListener('click', this.offClickHandler);
+	setTimeout(() => {
+		document.addEventListener('click', this.offClickHandler);
+	}, 0); 
   }
 
   public ngOnInit(): any {
